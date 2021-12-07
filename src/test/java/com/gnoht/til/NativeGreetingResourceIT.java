772e4e591b6bfecfa46d3867e456cfa73880e471
Thong Nguyen <thong@Guava.local>
2021-12-06 23:17:05 -0800

    Initial import

---
+++ src/test/java/com/gnoht/til/NativeGreetingResourceIT.java
@@ -0,0 +1,10 @@
+package com.gnoht.til;
+
+import com.gnoht.til.GreetingResourceTest;
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
